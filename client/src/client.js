65447e685764eda3fe0b2f014cb36871fc7be485
aryanjain200425 <46764031+aryanjain200425@users.noreply.github.com>
2021-06-14 11:02:12 -0400

    Add files via upload

---
+++ client/src/client.js
@@ -0,0 +1,29 @@
+const sock = io();
+
+const write = (text) => {
+  const parent = document.querySelector('#events');
+
+  const el = document.createElement('li');
+  el.innerHTML = text;
+
+  parent.appendChild(el);
+};
+
+
+const onFormSumbit = (e) => {
+  e.preventDefault();
+  console.log("done");
+  const input = document.querySelector("#chat");
+  const text = input.value;
+  input.value = "";
+
+  sock.emit('message', text);
+
+};
+
+
+write("Welcome to RPS");
+
+sock.on('message', write)
+
+document.querySelector("#chat-form").addEventListener('submit', onFormSumbit);
