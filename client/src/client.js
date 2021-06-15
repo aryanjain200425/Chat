a04cbcb46a5ad4b705626882c638cda5e0f6a209
aryanjain200425 <46764031+aryanjain200425@users.noreply.github.com>
2021-06-15 15:46:25 -0400

    Update client.js

--- client/src/client.js
+++ client/src/client.js
@@ -1,4 +1,4 @@
-const sock = io("https://git.heroku.com/sleepy-fortress-48260.git");
+const sock = io("https://sleepy-fortress-48260.herokuapp.com/");
 
 const write = (text) => {
   const parent = document.querySelector('#events');
