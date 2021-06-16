1244c2584699acebaaade3531808675ef32eeb99
aryanjain200425 <46764031+aryanjain200425@users.noreply.github.com>
2021-06-16 14:07:19 -0400

    Add files via upload

--- client/src/client.js
+++ client/src/client.js
@@ -1,4 +1,4 @@
-const sock = io("https://shielded-lowlands-10094.herokuapp.com/");
+const sock = io("https://sleepy-peak-88211.herokuapp.com");
 
 const write = (text) => {
   const parent = document.querySelector('#events');
