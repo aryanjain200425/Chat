63e2149dae238bd4dff449b9ef2813f69f6bb1eb
aryanjain200425 <46764031+aryanjain200425@users.noreply.github.com>
2021-06-16 13:34:16 -0400

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,14 @@
 const http = require('http');
 const express = require('express');
 
-const socketio = require('socket.io');
-
 const app = express();
+
+const io = require("socket.io")(app, {
+  cors: {
+    origin: "https://practical-swartz-1e6fb8.netlify.app",
+    methods: ["GET", "POST"]
+  }
+});
 
 const clientPath = `${__dirname}/../client`;
 console.log(`Serving static form ${clientPath}`);
@@ -14,7 +19,7 @@ app.use(express.static(clientPath));
 const server = http.createServer(app);
 
 
-const io = socketio(server);
+
 
 io.on('connection', (sock) => {
   console.log("Someone connected");
@@ -30,6 +35,9 @@ io.on('connection', (sock) => {
 server.on('error', (err) =>{
   console.log("Server Error:" , err);
 });
+
+
+
 
 
 server.listen(8080, () => {
